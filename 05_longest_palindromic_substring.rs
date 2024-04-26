b5a4cb7241473848d699958f85a11a0a4e07d527
Erick <ericklagreat@gmail.com>
2024-04-26 16:31:35 -0400

    Still needs work

--- 05_longest_palindromic_substring.rs
+++ 05_longest_palindromic_substring.rs
@@ -5,9 +5,9 @@ impl Solution {
         let mut max_dist: usize = 0;  
 
         //determine if odd or even, then set midpoint
-        let midpoint: usize = match s.len() % 2{
+        let midpoint: usize = match (s.len() + 2) % 2{
             0 => (s.len() / 2) - 1,
-            1 if s.len() == 1 => 1,
+            1 if s.len() == 1 => 1, //redundant
             1 => s.len() / 2,
             _ => 0,
         };
@@ -37,7 +37,6 @@ impl Solution {
         while checkpoint < s.len() - 1 {
             println!("MID: {}", checkpoint);
             for _i in 1..s.len() - checkpoint{
-                println!("I: {}", _i); 
                 if (checkpoint - _i) >= 0 {
                     println!("Elem: {}|{}",checkpoint - _i, s.chars().nth(checkpoint - _i).unwrap());
                 }
@@ -47,11 +46,34 @@ impl Solution {
             }
             checkpoint += 1;
         }
-
+        checkpoint = midpoint;
+        
         //even check
-    
+        println!("\nENETERING EVEN CHECK!\n"); 
+        //left
+        println!("Checking left side");
+        while checkpoint > 0 {
+            println!("MID: {}", checkpoint);
+            for _i in 0..checkpoint {
+                 println!("Elem: {}|{}",checkpoint + _i, s.chars().nth(checkpoint + _i).unwrap());
+                 println!("Elem: {}|{}",checkpoint - _i - 1, s.chars().nth(checkpoint - _i - 1).unwrap());
+            }
+            checkpoint -= 1;
+        }
 
 
+        checkpoint = midpoint + 1;
+
+        //right 
+        println!("Checking right side");
+        while checkpoint < s.len() - 1{
+            println!("MID: {}", checkpoint);
+            for _i in 0..s.len() - checkpoint {
+                println!("Elem: {}|{}",checkpoint + _i, s.chars().nth(checkpoint + _i).unwrap());
+                println!("Elem: {}|{}",checkpoint - _i - 1, s.chars().nth(checkpoint - _i - 1).unwrap());
+            } 
+            checkpoint += 1;
+        }
 
         //return slice of longest
         return "nothing".to_string()
