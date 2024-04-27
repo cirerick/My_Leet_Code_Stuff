20c634657eff4fc4128b8e86fbbc0df8ce386d47
Erick <ericklagreat@gmail.com>
2024-04-27 18:33:42 -0400

    343 ms, could be improved.

--- 05_longest_palindromic_substring.rs
+++ 05_longest_palindromic_substring.rs
@@ -1,5 +1,9 @@
 impl Solution {
     pub fn longest_palindrome(s: String) -> String {
+        //no point in moving forward when there is only one char
+        if s.len() == 1 {
+            return s
+        }
         //store positions of longest palindrome
         let mut interval: (usize, usize) = (0,0);
         let mut max_dist: usize = 0;  
@@ -7,7 +11,6 @@ impl Solution {
         //determine if odd or even, then set midpoint
         let midpoint: usize = match (s.len() + 2) % 2{
             0 => (s.len() / 2) - 1,
-            1 if s.len() == 1 => 1, //redundant
             1 => s.len() / 2,
             _ => 0,
         };
@@ -18,45 +21,73 @@ impl Solution {
 
         //odd palindrome check
         //left
-        println!("Checking left side");
-        while checkpoint > 0 {
-            println!("MID: {}", checkpoint);
+        println!("=====Checking left side:======");
+        //if the possible max has been reached from the previous iteration, stop.
+        while checkpoint > 0 && max_dist < (checkpoint * 2) {
+            println!("CHKPT: {}", checkpoint);
             for _i in 1..checkpoint + 1 {
-                if (checkpoint - _i) >= 0 {
+                if  s.chars().nth(checkpoint - _i).unwrap() == s.chars().nth(checkpoint + _i).unwrap() {
                     println!("Elem: {}|{}",checkpoint - _i, s.chars().nth(checkpoint - _i).unwrap());
-                } 
-                if (checkpoint + _i) <= s.len() - 1{
                     println!("Elem: {}|{}",checkpoint + _i, s.chars().nth(checkpoint + _i).unwrap());
+                    if ((checkpoint + _i) - (checkpoint - _i)) > max_dist {
+                        interval = (checkpoint - _i, checkpoint + _i);
+                        max_dist = interval.1 - interval.0;
+                        println!("\nINT: {:?}", interval);
+                        println!("MAX: {}\n", max_dist);  
+                    }  
+                } else {
+                    break;
                 } 
             }
             checkpoint -= 1;
         }
+
+        
         //right
-        println!("\nchecking right side");
+        println!("\n=====Checking right side:=====");
         checkpoint = midpoint + 1; //adding one as we had already checked midpoint to the right
-        while checkpoint < s.len() - 1 {
-            println!("MID: {}", checkpoint);
+        while checkpoint < s.len() && max_dist < ((s.len() - checkpoint - 1) * 2) {
+            println!("CHKPT: {}", checkpoint);
             for _i in 1..s.len() - checkpoint{
-                if (checkpoint - _i) >= 0 {
+                if s.chars().nth(checkpoint - _i).unwrap() == s.chars().nth(checkpoint + _i).unwrap(){
                     println!("Elem: {}|{}",checkpoint - _i, s.chars().nth(checkpoint - _i).unwrap());
-                }
-                if (checkpoint + _i) <= s.len() - 1{
                     println!("Elem: {}|{}",checkpoint + _i, s.chars().nth(checkpoint + _i).unwrap());
+                    if ((checkpoint + _i) - (checkpoint - _i)) > max_dist {
+                        interval = (checkpoint - _i, checkpoint + _i);
+                        max_dist = interval.1 - interval.0;
+                        println!("\nINT: {:?}", interval);
+                        println!("MAX: {}\n", max_dist);  
+                    }
+                } else {
+                    break;
                 }
             }
             checkpoint += 1;
         }
+
+        
+
         checkpoint = midpoint;
         
         //even check
         println!("\nENETERING EVEN CHECK!\n"); 
         //left
-        println!("Checking left side");
-        while checkpoint > 0 {
-            println!("MID: {}", checkpoint);
+        println!("=====Checking left side:======");
+        while checkpoint > 0 && max_dist < (checkpoint * 2) {
+            println!("CHKPT: {}", checkpoint);
             for _i in 0..checkpoint {
-                 println!("Elem: {}|{}",checkpoint + _i, s.chars().nth(checkpoint + _i).unwrap());
-                 println!("Elem: {}|{}",checkpoint - _i - 1, s.chars().nth(checkpoint - _i - 1).unwrap());
+                if s.chars().nth(checkpoint + _i).unwrap() == s.chars().nth(checkpoint - _i - 1).unwrap(){
+                    println!("Elem: {}|{}",checkpoint + _i, s.chars().nth(checkpoint + _i).unwrap());
+                    println!("Elem: {}|{}",checkpoint - _i - 1, s.chars().nth(checkpoint - _i - 1).unwrap());
+                    if ((checkpoint + _i) - (checkpoint - _i - 1)) > max_dist {
+                        interval = (checkpoint - _i - 1, checkpoint + _i);
+                        max_dist = interval.1 - interval.0;
+                        println!("\nINT: {:?}", interval);
+                        println!("MAX: {}\n", max_dist);  
+                    }
+                }else{
+                    break;
+                }
             }
             checkpoint -= 1;
         }
@@ -65,17 +96,31 @@ impl Solution {
         checkpoint = midpoint + 1;
 
         //right 
-        println!("Checking right side");
-        while checkpoint < s.len() - 1{
-            println!("MID: {}", checkpoint);
+        println!("\n=====Checking right side:=====");
+        while checkpoint < s.len() && max_dist < (s.len() - checkpoint) * 2 {
+            println!("CHKPT: {}", checkpoint);
             for _i in 0..s.len() - checkpoint {
-                println!("Elem: {}|{}",checkpoint + _i, s.chars().nth(checkpoint + _i).unwrap());
-                println!("Elem: {}|{}",checkpoint - _i - 1, s.chars().nth(checkpoint - _i - 1).unwrap());
+                if s.chars().nth(checkpoint + _i).unwrap() == s.chars().nth(checkpoint - _i - 1).unwrap() {
+                    println!("Elem: {}|{}",checkpoint + _i, s.chars().nth(checkpoint + _i).unwrap());
+                    println!("Elem: {}|{}",checkpoint - _i - 1, s.chars().nth(checkpoint - _i - 1).unwrap());
+                    if ((checkpoint + _i) - (checkpoint - _i - 1)) > max_dist {
+                        interval = (checkpoint - _i - 1, checkpoint + _i);
+                        max_dist = interval.1 - interval.0; 
+                        println!("\nINT: {:?}", interval);
+                        println!("MAX: {}\n", max_dist);  
+                    }
+                } else {
+                    break;
+                }
             } 
             checkpoint += 1;
         }
 
         //return slice of longest
-        return "nothing".to_string()
+        if max_dist > 0 {
+            return (&s[interval.0..interval.1 + 1]).to_string()
+        } else {
+            return (&s[0..1]).to_string()
+        }
     }
 }
